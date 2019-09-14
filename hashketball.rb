0415d4b7bfd0ff171044243c2ad9bac8c3f1681b
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 16:01:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -239,6 +239,7 @@ def player_with_longest_name
   game_hash.each do |place_hash, team_hash|
     team_hash[:players].each do |player_info|
     binding.pry
+    
     end  
   end
   # game_hash.each do |place_hash, team_hash|

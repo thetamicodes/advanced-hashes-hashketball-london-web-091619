fe68f26e44968ef6c110fae92bc22ce8346c664f
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 11:16:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,6 +195,8 @@ def player_stats(player_name)
 end
 
 def big_shoe_rebounds
+game[:home][:players][:shoe] = 0
+
  game_hash.each do |place, team|
   team.each do |attributes, data|
     if attributes == :players

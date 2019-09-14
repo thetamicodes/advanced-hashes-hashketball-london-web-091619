f88afe2a392a99f79a1b1ec1dd28511f38944383
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 14:37:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -124,7 +124,6 @@ def num_points_scored(player_name)
       if team_info == :players
         details.each do |player_info|
          if player_info[:player_name] == player_name 
-           binding.pry
           return player_info[:points]
          end    
         end   

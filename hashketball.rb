b5e800f5574ebba69c6dbd136fcc224fdf38e093
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 15:42:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -241,8 +241,7 @@ def player_with_longest_name
       if team_info == :players
         details.each do |player_info|
           longest_name << player_info[:player_name]
-          return longest_name.max_by
-    binding.pry
+          return longest_name.max_by(&length)
         end
       end
     end

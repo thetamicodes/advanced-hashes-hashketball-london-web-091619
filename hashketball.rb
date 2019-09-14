ed7fbeae0ff6c213f70ed32ce6de7b28592c034e
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 16:33:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -255,7 +255,9 @@ def long_name_steals_a_ton
       if team_info == :players
         details.each do |player_info|
           most_steals << player_info[:steals]
-          moste_steals.sort 
+          most_steals.sort 
+          most_steals[-1]
+          binding.pry
         end
       end
     end      
